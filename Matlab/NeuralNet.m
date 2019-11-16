a7c6b8c7b783479feb5f7d83f78a37ae47a38e4d
Richard Harris <rkharris12@gmail.com>
2019-11-15 18:31:59 -0800

    trained relu neural net

--- Matlab/NeuralNet.m
+++ Matlab/NeuralNet.m
@@ -43,27 +43,18 @@ end
 function Yp=BackpropagationNetwork(X,Y)
     [Nsamps D]=size(X); % Nsamps is number of samples (2240), D is dimension (256)
     N=D; M=10; % number of input layer nodes and output layer nodes, respectively
-    L=20; % number of hidden layer nodes
-    % convert x to a value from 0 to 1
-    for d=1:D
-        xmin=min(X(:,d));
-        xmax=max(X(:,d));
-        s=1/(xmax-xmin);
-        for i=1:Nsamps
-            X(i,d)=(X(i,d)-xmin)*s;  % Convert to entire dynamic range  
-        end
-    end
+    L=30; % number of hidden layer nodes
+    % convert X to Q16.  Shift right by 16
+    X = X/65536;
     syms x
-    g=(2/(1+exp(-x)) - 1);    % Sigmoid activation function
-    dg=diff(g);               % its derivative function
-    g=matlabFunction(g);
-    dg=matlabFunction(dg);
-    Whnew=2*rand(N+1,L)-ones(N+1,L); % (257x20) randomly initialize N+1 dimensional (includes bias b) augmented hidden weight vectors Wh=[wh1 wh2...whL].  Values between -1 and 1
-    Wonew=2*rand(L+1,M)-ones(L+1,M); % (21x10) randomly initialize L+1 dimensional (includes bias b) augmented output weight vectors Wh=[wo1 wo2...woM].  Values between -1 and 1
-    eta=0.1; % learning rate
+    Whnew=(2*rand(N+1,L)-ones(N+1,L))/2; % (257x20) randomly initialize N+1 dimensional (includes bias b) augmented hidden weight vectors Wh=[wh1 wh2...whL].  Values between -0.5 and 0.5
+    Wonew=(2*rand(L+1,M)-ones(L+1,M))/2; % (31x10) randomly initialize L+1 dimensional (includes bias b) augmented output weight vectors Wh=[wo1 wo2...woM].  Values between -0.5 and 0.5
+    eta=0.01; % learning rate
     tolerance=2*10^-2;
     error=inf;
     iter=0;
+    dgo = ones(M,1);
+    dgh = ones(L,1);
     while error>tolerance
         iter=iter+1;
         Whold=Whnew;
@@ -75,33 +66,70 @@ function Yp=BackpropagationNetwork(X,Y)
         
         % forward pass
         ah=Whold'*xtrain; % activation (net input) of hidden layer
-        z=[1;g(ah)]; % augmented output of hidden layer
+        for i=1:length(ah) % Relu activation function
+            if ah(i) < 0
+                ah(i) = 0;
+                dgh(i) = 0;
+            else
+                dgh(i) = 1;
+            end
+        end
+        z=[1;ah]; % augmented output of hidden layer
         ao=Woold'*z; % activation (net input) of output layer
-        yp=g(ao)'; % output of output layer
+        for i=1:length(ao) % Relu activation function
+            if ao(i) < 0
+                ao(i) = 0;
+                dgo(i) = 0;
+            else
+                dgo(i) = 1;
+            end
+        end
+        yp=ao'; % output of output layer
         
         % backward error propagation
-        do=(ytrain'-yp').*dg(ao); % find d of output layer (Mx1 vector)
-        dh=(Woold(2:L+1,1:M)*do).*dg(ah); % find d of hidden layer (Lx1 vector).  Remove the first row of Wo: the bias offset
+        do=(ytrain'-yp').*dgo; % find d of output layer (Mx1 vector)
+        dh=(Woold(2:L+1,1:M)*do).*dgh; % find d of hidden layer (Lx1 vector).  Remove the first row of Wo: the bias offset
         Wonew=Woold+(eta*do*z')'; % update weights of output layer
         Whnew=Whold+(eta*dh*xtrain')'; % update weights of hidden layer
         
-        if ~mod(iter,1000) % check error every 1000 iterations
-            Yp=g(Wonew'*[ones(1,Nsamps); g(Whnew'*[ones(Nsamps,1) X]')])'; % forward pass to get output Yp given X
+        if ~mod(iter,100000) % check error every 1000 iterations
+            hidden = relu(Whnew'*[ones(Nsamps,1) X]');
+            Yp=relu(Wonew'*[ones(1,Nsamps); hidden])'; % forward pass to get output Yp given X
             % convert Y and Yp to 1-D integer labels so my confusion matrix
             % function can work on them
             Ynew=zeros(Nsamps,1);
             Ypnew=zeros(Nsamps,1);
             for i=1:Nsamps
                 Ynew(i)=find(Y(i,:)==1);
-                Ypnew(i)=find(Yp(i,:)==max(Yp(i,:)));
+                %find(Yp(i,:)==max(Yp(i,:)))
+                val = find(Yp(i,:)==max(Yp(i,:)));
+                if length(val) ~= 1
+                    Ypnew(i) = randi(10);
+                else
+                    Ypnew(i) = val;
+                end
             end
             [Cm error]=ConfusionMatrix(Ynew,Ypnew);
         end
     end
-    Yp=g(Wonew'*[ones(1,Nsamps); g(Whnew'*[ones(Nsamps,1) X]')])'; % forward pass to get output Yp given X
+    hidden = relu(Whnew'*[ones(Nsamps,1) X]');
+    Yp=relu(Wonew'*[ones(1,Nsamps); hidden])'; % forward pass to get output Yp given X
 end
 
 
+function ret=relu(nodes) % compute relu for nodes for all samples (nodes is 30x2240 for hidden)
+    [D Nsamps]=size(nodes); % Nsamps is number of samples, D is dimension
+    ret = ones(D, Nsamps);
+    for i = 1:D
+       for j = 1:Nsamps
+           if nodes(i,j) < 0
+               ret(i,j) = 0;
+           else
+               ret(i,j) = nodes(i,j);
+           end
+       end
+    end
+end
 
 
 function showImage(mk) % displays image.  Input is a single row of X (1x256)
--- Matlab/classify.m
+++ Matlab/classify.m
@@ -1,15 +1,35 @@
-load hidden_weights.mat;
-load output_weights.mat;
+%% Sigmoid activation function
+%load hidden_weights.mat;
+%load output_weights.mat;
 
-X=load("data1.txt"); 
-xtrain = X(1,:)';
+%syms x
+%g=(2/(1+exp(-x)) - 1);    % Sigmoid activation function
+%g=matlabFunction(g);
+
+% ah=Wh'*xtrain; % activation (net input) of hidden layer
+% z=[1;g(ah)]; % augmented output of hidden layer
+% ao=Wo'*z; % activation (net input) of output layer
+% yp=g(ao)'; % output of output layer
+
+%% Relu activation function
+load weights.mat;
+
+X=load("data1.txt");
+X = X/65536; % convert X to Q16.  Shift right by 16
+xtrain = X(4,:)';
 xtrain = [1;xtrain];
 
-syms x
-g=(2/(1+exp(-x)) - 1);    % Sigmoid activation function
-g=matlabFunction(g);
-
-ah=Wh'*xtrain; % activation (net input) of hidden layer
-z=[1;g(ah)]; % augmented output of hidden layer
-ao=Wo'*z; % activation (net input) of output layer
-yp=g(ao)'; % output of output layer
+ah=Whnew'*xtrain; % activation (net input) of hidden layer
+for i=1:length(ah) % Relu activation function
+    if ah(i) < 0
+        ah(i) = 0;
+    end
+end
+z=[1;ah]; % augmented output of hidden layer
+ao=Wonew'*z; % activation (net input) of output layer
+for i=1:length(ao) % Relu activation function
+    if ao(i) < 0
+        ao(i) = 0;
+    end
+end
+yp=ao'; % output of output layer
